3ebcf8e6dfe37f5e7a31f38d6780e5035a47bf04
Farad <sanmartin.espinoza.alejandro@gmail.com>
2019-05-30 20:25:09 -0500

    Agregando un fragment por medio de codigo y haciendo uso del framelayout

--- app/src/main/java/com/app/cursofragmentscf/MainActivity.java
+++ app/src/main/java/com/app/cursofragmentscf/MainActivity.java
@@ -1,7 +1,11 @@
 package com.app.cursofragmentscf;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.app.cursofragmentscf.Ejemplo_1.TwoFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TwoFragment codeFragment = new TwoFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.contenedorFrame,codeFragment);
+        fragmentTransaction.commit();
     }
-    //probando comentario
+
 }
